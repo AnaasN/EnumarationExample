dedc222e9de3b3977c3326bcb161fa0b62b30285
AnaasN <nice-anaas@hotmail.co.uk>
2016-12-07 21:32:25 +0000

    Add files via upload
    Not worth it.

---
+++ TeamWork/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Increment;
+
+            Console.WriteLine("Enter the number:");
+            Increment = int.Parse(Console.ReadLine());
+
+            for (int Inc = 0; Inc < 28; Inc++)
+            {
+                Console.WriteLine(DayRollOver(EnumDay.Thursday, Inc));
+                if (Inc == 7) Console.WriteLine("\n");
+            }
+
+        }
+        enum EnumDay : int
+        {
+            Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday
+        }
+        static EnumDay DayRollOver(EnumDay day, int userinput)
+        {
+            int x = ((userinput + (int)day) % 7);
+            return (EnumDay)x;
+        }
+
+    }
+
+}
+
